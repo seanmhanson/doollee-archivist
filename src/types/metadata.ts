d11a07059dcc01b1e8dae5acdd469c9f48270820
Seán Hanson <smh@seanmhanson.com>
2025-12-08 13:21:39 -0500

    Add initial types and schemas for MongoDB integration

---
+++ src/types/metadata.ts
@@ -0,0 +1,15 @@
+import type { ObjectId } from "mongodb";
+
+export type Metadata = {
+  _id: ObjectId;
+  scrapedAt: Date;
+  sourceUrl: string;
+};
+
+export const requiredMetadataFields = ["_id", "scrapedAt", "sourceUrl"];
+
+export const metadataProperties = {
+  _id: { bsonType: "objectId" },
+  scrapedAt: { bsonType: "date" },
+  sourceUrl: { bsonType: "string" },
+};
