17dee84d7d24cf81cc0e9f88866cffb073b87d49
Seán Hanson <smh@seanmhanson.com>
2026-02-02 14:10:22 -0500

    Add a debounce util with force and clear fns

---
+++ src/utils/debounce.ts
@@ -0,0 +1,38 @@
+/**
+ * Utility to set a debounce on a function call to prevent multiple frequent calls, and
+ * provide helpers to force execution or clear pending calls.
+ * @param fn the function to be debounced
+ * @param delay the delay in ms to enforce between calls
+ */
+function debounce<T extends (...args: any[]) => any>(fn: T, delay: number) {
+  let timeout: NodeJS.Timeout | null = null;
+
+  const debouncedFn = function (...args: Parameters<T>): void {
+    clear();
+
+    timeout = setTimeout(() => {
+      fn(...args);
+      timeout = null;
+    }, delay);
+  };
+
+  const forcedFn = function (...args: Parameters<T>): ReturnType<T> {
+    clear();
+    return fn(...args);
+  };
+
+  const clear = () => {
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = null;
+    }
+  };
+
+  return {
+    debouncedFn,
+    forcedFn,
+    clear,
+  };
+}
+
+export default debounce;
